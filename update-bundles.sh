b707fd0ba3db7e52c592ba36d0ec8ae4a3e25e9a
gregwilliams <gregwilliams@deephaven.io>
2020-05-20 12:53:34 -0700

    vimrc file

---
+++ update-bundles.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -e
+(
+	cd vim/bundle
+	for submodule in *; do
+		if [ -e "$submodule/.git" ]; then
+			(
+				cd "$submodule"
+				echo "===> $submodule"
+
+				if git branch | grep -q develop; then
+					git checkout --quiet develop
+				else
+					git checkout --quiet master
+				fi
+				git pull --quiet
+			)
+		fi
+	done
+)
